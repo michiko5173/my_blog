fa7ac5901ad251444a8db3dcdbc21df8a4eea274
ywj <michiko5173@gmail.com>
2018-07-11 17:12:44 +0800

    ''2

--- mysite2/settings.py
+++ mysite2/settings.py
@@ -77,15 +77,10 @@ WSGI_APPLICATION = 'mysite2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'mysite',
-        'USER':'postgres',
-        'PASSWORD':'Yang@00o00',
-        'HOST':'',
-        'PORT':'',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
